095a65829551297db307b9e4f939717ebebaf669
清水紀行 <noriyuki.shimizu0612@gmail.com>
2023-10-28 21:39:36 +0900

    reset css を変更

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css'
+    }
+  ]
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  css: ['@/assets/styles/main.css'],
   devtools: { enabled: true },
   plugins: ['@/plugins/rest-client/index.ts'],
   modules: ['@vueuse/nuxt']
